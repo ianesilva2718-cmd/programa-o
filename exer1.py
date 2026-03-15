cafd2d5e70f9e1e765f1b5da3dd63a279b832825
ianesilva2718-cmd <ianesilva2718@gmail.com>
2026-03-15 20:40:59 -0300

    add: exer1.py

---
+++ exer1.py
@@ -0,0 +1,9 @@
+nome = input("Digite o nome do aluno: ")
+idade = input("Digite a idade: ")
+altura = input("Digite a altura: ")
+contato = input("Digite o contato: ")
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Contato:", contato)
