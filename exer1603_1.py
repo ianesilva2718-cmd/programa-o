6ca7214d6d10552db1b430f5266afb8ab1b0ddcd
Iane Silva <iane62234336@alunos.sc.senac.br>
2026-03-16 20:34:23 -0300

    exer1603_1.py

---
+++ exer1603_1.py
@@ -0,0 +1,16 @@
+lista_compras = []
+
+lista_compras.append("maçã")
+print("Após adicionar maçã:", lista_compras)
+
+lista_compras.append("pera")
+print("Após adicionar pera:", lista_compras)
+
+lista_compras.append("melão")
+print("Após adicionar melão:", lista_compras)
+
+lista_compras.remove("maçã")
+print("Após remover maçã:", lista_compras)
+
+lista_compras.remove("melão")
+print("após remover melão:", lista_compras)
