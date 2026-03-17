ac21bbe317dfe62f28b04e16eef13b6133181986
Iane Silva <iane62234336@alunos.sc.senac.br>
2026-03-16 21:24:55 -0300

    tuple

---
+++ exer1603_3.py
@@ -0,0 +1,8 @@
+
+dias = ("segunda", "terça","quarta", "quinta","sexta")
+print(dias)
+
+print("o primeiro dia da semana é:", dias[0])
+print("o ultimo dia da semana é:", dias[-1])
+
+print("a tupla tem", len(dias),"elementos")
