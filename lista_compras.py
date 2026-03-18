ce2fd7f03de9c1e8c80453b55b1583543ee6f144
ianesilva2718-cmd <iane62234336@alunos.sc.senac.br>
2026-03-18 20:30:55 -0300

    exercicio3

--- lista_compras.py
+++ lista_compras.py
@@ -1,6 +1,3 @@
-lista_compras = ["arroz", "feijão", "macarrão", "leite"]
-
-# 2. Mostramos a lista atual para o usuário
-print("Lista de compras inicial:")
-print(lista_compras)
-
+lista_compras = ["arroz", "feijão", "macarrão", "leite"]  
+# 2. Mostramos a lista atual para o usuário 
+print("Lista de compras inicial:") print(lista_compras)
